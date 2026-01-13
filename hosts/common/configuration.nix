504c3cfc5cdd8aa9be3d0492e5db149a4b89d9ec
dofi4ka <dofi4ka@yandex.ru>
2026-01-13 20:29:22 +0300

    feat: Add bluetooth and blueman
    Closes: #22

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -18,6 +18,22 @@
     };
   };
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
+  services.blueman.enable = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
