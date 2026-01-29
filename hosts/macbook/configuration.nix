e267e627f2136a2ac78ad3a799c2c2f184952a70
dofi4ka <dofi4ka@yandex.ru>
2026-01-29 03:20:53 +0300

    feat(macbook): Enable bluetooth

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -21,7 +21,22 @@
     };
   };
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
   services = {
+    blueman.enable = true;
     greetd = {
       enable = true;
       settings.default_session = {
